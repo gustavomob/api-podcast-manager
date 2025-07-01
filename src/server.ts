686630678a6c202e1f50acf42c6f6fd66c2fb97e
gustavogss <gustavogss.dev@gmail.com>
2025-06-30 22:03:25 -0300

    feat: create server http

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,15 @@
-console.log("Hello World!");
+import * as dotenv from "dotenv";
+import * as http from "http"
+
+dotenv.config();
+
+const PORT = process.env.PORT;
+
+const server = http.createServer((request: http.IncomingMessage, response: http.ServerResponse) => {
+  response.end("Servidor OK");
+});
+
+server.listen(PORT, () => {
+  console.log(`Servidor iniciado na Porta ${PORT}`);
+});
+
